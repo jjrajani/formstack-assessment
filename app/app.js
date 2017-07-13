e79e1749fed18f606ddff031c97b1de5ac0d0f89
humdrum <jjrajani@gmail.com>
2017-07-13 13:21:17 -0400

    remove routes

--- app/app.js
+++ app/app.js
@@ -2,12 +2,5 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
   'myApp.home',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/home'});
-}]);
+]);
